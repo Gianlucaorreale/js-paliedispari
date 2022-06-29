fc79602a095fc885fee10399152eabc79f8810cc
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-06-29 15:47:55 +0200

    add-even-and-odd

--- js/even-and-odd.js
+++ js/even-and-odd.js
@@ -5,4 +5,25 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
- 
+const evenAndOdd = prompt ('scegli: pari o dispari');
+const userNumber = parseInt(prompt ('inserisci un numero da 1 a 5'));
+console.log("l'utente sceglie : " + evenAndOdd);
+console.log("il numero dell'utente è : " + userNumber);
+
+function getRandomNumber () {
+    const cpuRandomNumber = Math.floor((Math.random() * 5) + 1);
+    console.log ('il numero del cpu è : ' + cpuRandomNumber);
+    return cpuRandomNumber
+}
+
+const sum = userNumber + getRandomNumber();
+console.log( 'la somma dei numeri è ' + sum);
+
+function getEvenAndOdd () {
+    if (sum % 2 === 0){
+        console.log('il risultato della somma è : pari')
+    } else {
+        console.log('il risultato della somma è : dispari')
+    }
+}
+
